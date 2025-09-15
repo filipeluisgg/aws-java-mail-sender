9ac6c2da488710000ba7f90e55c21c3b8fa43778
Luis Felipe <filipeluisgg+github@gmail.com>
2025-09-15 19:19:24 -0300

    first commit with the base structure of the email microservice configured

---
+++ email/src/main/java/com/felipe/email/EmailApplication.java
@@ -0,0 +1,15 @@
+package com.felipe.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(EmailApplication.class, args);
+    }
+}
+
+//Apenas ASSISTIR a última aula (aula 13) e implementar o envio de email sozinho com AWS SES!
+

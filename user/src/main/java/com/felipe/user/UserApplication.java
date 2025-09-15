1fa6a145c6991ccd470bdf799f2aeff7bda24fb3
Luis Felipe <filipeluisgg+github@gmail.com>
2025-09-15 19:33:38 -0300

    feat: add base structure of `user` microsservice

---
+++ user/src/main/java/com/felipe/user/UserApplication.java
@@ -0,0 +1,15 @@
+package com.felipe.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+}
+
+//Apenas ASSISTIR a última aula (aula 13) e implementar o envio de email sozinho com AWS SES!
+
